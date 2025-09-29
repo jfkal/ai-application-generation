97bdd1d23826b32475ba64956b7144de73f6e9ba
jfkal <2380768433@qq.com>
2025-09-29 22:44:52 +0800

    AI 零代码生成应用 - 用户模块后端
    1. 开发MyBatis Flex 代码生成器
    2. 开发核心功能
    3. 全局自定义请求 + 前端代码生成器
    4. 清理多余代码

--- src/main/java/com/yuanfen/aiapplicationgeneration/AiApplicationGenerationApplication.java
+++ src/main/java/com/yuanfen/aiapplicationgeneration/AiApplicationGenerationApplication.java
@@ -1,9 +1,11 @@
 package com.yuanfen.aiapplicationgeneration;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yuanfen.aiapplicationgeneration.mapper")
 public class AiApplicationGenerationApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/yuanfen/aiapplicationgeneration/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.yuanfen.aiapplicationgeneration.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.yuanfen.aiapplicationgeneration.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="http://111.230.108.171/">程序员缘分</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
